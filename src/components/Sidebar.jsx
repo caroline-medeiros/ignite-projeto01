55c75e4a1e92b4a82bd5cb871533365afc36f9d3
Caroline Medeiros <carol02medeiros@gmail.com>
2022-06-14 18:09:37 -0300

    estilizacao dos componentes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,21 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
 
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img src="https://images.unsplash.com/photo-1487611459768-bd414656ea10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"  />
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1487611459768-bd414656ea10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"  />
 
     <div className={styles.profile}>
+      <img className={styles.avatar} src="https://github.com/caroline-medeiros.png" />
       <strong>Caroline Medeiros</strong>
       <span>Web Developer</span>
     </div>
     <footer>
       <a href="#">
+        <PencilLine size={20}/>
+
         Editar seu perfil
       </a>
     </footer>
